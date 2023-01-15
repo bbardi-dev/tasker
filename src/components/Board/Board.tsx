49a052b30793e564db63622f56a64b51f553a5ee
Zoltan Kerekes <zoltankerekes0928@gmail.com>
2023-01-15 19:42:55 +0100

    "+ Add Task" added

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -9,20 +9,25 @@ const Board = ({data}: Props) => {
         <h2 className="text-xl mb-4"> PreTodo</h2>
         <div className="bg-gray-300  text-center rounded">
           <Task data={data}/>
+          <button className="text-gray-700 mb-4">+ Add Task</button>
         </div>
+        
       </div>
      <div className="m-4 ">
         <h2 className="text-xl mb-4"> Todo</h2>
         <div className="bg-gray-300  text-center rounded">
           <Task data={data}/>
+          <button className="text-gray-700 mb-4">+ Add Task</button>
         </div>
       </div>
       <div className="m-4">
         <h2 className="text-xl mb-4"> Done</h2>
         <div className="bg-gray-300  text-center rounded">
           <Task data={data}/>
+          <button className="text-gray-700 mb-4">+ Add Task</button>
         </div>
       </div>
+      
     </div>
   )
 };
